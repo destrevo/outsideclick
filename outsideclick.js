bafb892990b71741776a7079b83cf87fba3da0b3
maczko <maczko@insine.email>
2022-05-27 12:45:52 +0200

    fancy

--- outsideclick.js
+++ outsideclick.js
@@ -1,7 +1,7 @@
     const element = document.querySelector(".burger-options");
 
     document.addEventListener("mousedown", (e) => {
-        // console.log(concernedElement, e.target);
+        // console.log(element, e.target);
         if (element.contains(e.target)) {
             // console.log("clicked inside");
         } else {
@@ -9,3 +9,9 @@
             // console.log("clicked outside");
         }
     }
+/////////////////////// a little fancy
+    const element_fancy = document.querySelector("whatever");
+
+    document.addEventListener("mousedown", (e) => {
+        if (!element_fancy).contains(e.target)) setOpen(false)
+    });
